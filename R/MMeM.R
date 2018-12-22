6fdfc98c083b445ac839243cef89197dbe3562d2
pengluyaoyao <luyaopeng.cn@gmail.com>
2018-12-22 15:26:18 -0800

    updates

--- R/MMeM.R
+++ R/MMeM.R
@@ -1,5 +1,6 @@
 #' MMeM: Estimating the variance covariance components of the multivariate mixed effects model
 #'
+#'
 #' @author Luyao Peng \email{luyaopeng.cn@gmail.com}
 #' @author Rui Yang \email{ray.cn.us@gmail.com}
 #' @docType package
