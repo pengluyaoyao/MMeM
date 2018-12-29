363ab25a30b4b75211951adee763d217cc19f2b5
pengluyaoyao <luyaopeng.cn@gmail.com>
2018-12-29 00:35:57 -0800

    updates

--- R/MMeM_Henderson3.R
+++ R/MMeM_Henderson3.R
@@ -10,8 +10,6 @@
 #' @examples
 #' \dontrun{
 #' data(simdata)
-#' T.start = matrix(c(10,5,5,15),2,2)
-#' E.start = matrix(c(10,1,1,3),2,2)
 #' results_henderson = MMeM_henderson3(fml = c(V1,V2) ~ X_vec + (1|Z_vec),
 #' data = simdata, factor_X = TRUE)
 #' }
